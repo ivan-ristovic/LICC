04aab3ee04d0e6c877d8ee44101b20e3373ae630
Ivan Ristovic <ivo.ristovic@gmail.com>
2019-12-15 00:39:21 +0100

    [CAdapter-Tests] Expression tests refactor

--- RICC.Tests/AST/Builders/C/ExpressionTests.cs
+++ RICC.Tests/AST/Builders/C/ExpressionTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using RICC.AST.Nodes;
@@ -11,39 +12,49 @@ namespace RICC.Tests.AST.Builders.C
         public void LiteralExpressionTest()
         {
             // TODO support chars properly
-            ASTNode ast = CASTProvider.BuildFromSource("int x = 3; float y = 2.3; char c = 'a'; string s = \"abc\";");
-            AssertExpressionValuesFromDeclaration(ast, ("x", 3), ("y", 2.3), ("c", "'a'"), ("s", "abc"));
+            AssertExpressionValue("int x = 3", 3);
+            AssertExpressionValue("float y = 2.3", 2.3);
+            AssertExpressionValue("char c = 'a'", "'a'");
+            AssertExpressionValue("string s = \"abc\"", "abc");
         }
 
         [Test]
         public void ArithmeticExpressionTest()
         {
-            ASTNode ast = CASTProvider.BuildFromSource("int x = 1 << (1 + 1 * 2) >> 3; float y = 2.3 + 4.0 / 2.0; double c = 3.3 + (4.1 - 1.1) * 2.0;");
-            AssertExpressionValuesFromDeclaration(ast, ("x", 1), ("y", 4.3), ("c", 9.3));
+            AssertExpressionValue("int x = 1 << (1 + 1 * 2) >> 3", 1);
+            AssertExpressionValue("float y = 2.3 + 4.0 / 2.0", 4.3);
+            AssertExpressionValue("double z = 3.3 + (4.1 - 1.1) * 2.0", 9.3);
         }
 
         [Test]
         public void RelationalExpressionTest()
         {
-            ASTNode ast = CASTProvider.BuildFromSource("bool a = 1 > 1, b = 1 >= 1, c = 2 > 1, d = 3 < 1, e = 1 != 1, f = (1 + 1) == 2");
-            AssertExpressionValuesFromDeclaration(ast, ("a", false), ("b", true), ("c", true), ("d", false), ("e", false), ("f", true));
-
-            ast = CASTProvider.BuildFromSource("bool a = 1.1 > 1.0, b = 1.101 >= 1.1, c = (1 + 3 * 2) > 7, d = 3.0 + 0.1 > 3.0, e = 1.01 != 1.0, f = (1 << 1) == 2");
-            AssertExpressionValuesFromDeclaration(ast, ("a", true), ("b", true), ("c", false), ("d", true), ("e", true), ("f", true));
+            AssertExpressionValue("bool a = 1 > 1", false);
+            AssertExpressionValue("bool b = 1 >= 1", true);
+            AssertExpressionValue("bool c = 2 > 1", true);
+            AssertExpressionValue("bool d = 3 < 1", false);
+            AssertExpressionValue("bool e = 1 != 1", false);
+            AssertExpressionValue("bool f = (1 + 1) == 2", true);
+            AssertExpressionValue("bool g = 1.1 > 1.0", true);
+            AssertExpressionValue("bool h = 1.101 >= 1.1", true);
+            AssertExpressionValue("bool i = (1 + 3 * 2) > 7", false);
+            AssertExpressionValue("bool j = 3.0 + 0.1 > 3.0", true);
+            AssertExpressionValue("bool k = 1.01 != 1.0", true);
+            AssertExpressionValue("bool l = (1 << 1) == 2", true);
         }
 
 
-        private static void AssertExpressionValuesFromDeclaration(ASTNode ast, params (string Identifier, object value)[] vars)
+        private static void AssertExpressionValue(string decl, object expected)
         {
-            IEnumerable<DeclarationNode> decls = ast.Children.SelectMany(d => d.Children.ElementAt(1).As<DeclarationListNode>().Declarations);
-            Assert.That(decls.Select(var => ExtractIdentifierAndValue(var)), Is.EqualTo(vars).Within(1e-10));
-
-
-            static (string, object?) ExtractIdentifierAndValue(DeclarationNode declNode)
-            {
-                VariableDeclarationNode var = declNode.As<VariableDeclarationNode>();
-                return (var.Identifier, var.Initializer?.Evaluate());
-            }
+            ASTNode ast = CASTProvider.BuildFromSource(decl);
+            object value = ast.Children
+                .First().As<DeclarationStatementNode>()
+                .Children.ElementAt(1).As<DeclarationListNode>()
+                .Declarations
+                .First().As<VariableDeclarationNode>()
+                .Initializer?.Evaluate()
+                ?? throw new ArgumentException("Missing initializer in test");
+            Assert.That(value, Is.EqualTo(expected).Within(1e-10));
         }
     }
 }
