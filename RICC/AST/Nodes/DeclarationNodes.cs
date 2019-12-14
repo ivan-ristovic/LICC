eb11bfda1d3968661ba9c5340e1d451134cea9e7
Ivan Ristovic <ivo.ristovic@gmail.com>
2019-12-15 00:46:18 +0100

    [ASTNodes] Refactor

--- RICC/AST/Nodes/DeclarationNodes.cs
+++ RICC/AST/Nodes/DeclarationNodes.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using RICC.AST.Nodes.Common;
+using Serilog;
 
 namespace RICC.AST.Nodes
 {
+    public class DeclarationSpecifiersNode : ASTNode
+    {
+        public DeclarationSpecifiersFlags Specifiers { get; }
+        public string TypeName { get; }
+        public Type? Type { get; }
+
+
+        public DeclarationSpecifiersNode(int line, string type, IEnumerable<string> specs, ASTNode? parent = null)
+            : base(line, parent)
+        {
+            this.Specifiers = DeclarationSpecifiers.Parse(specs);
+            this.TypeName = type;
+            TypeCode? typeCode = Types.TypeCodeFor(type);
+            if (typeCode is null)
+                Log.Warning("Unknown type: {Type}", type);
+            else
+                this.Type = Types.ToType(typeCode.Value);
+        }
+    }
+
     public abstract class DeclarationNode : ASTNode
     {
         protected DeclarationNode(int line, IEnumerable<ASTNode> children, ASTNode? parent = null)
