dba56220cc1776778400ac1c55f0dc8c69f563f7
Ivan Ristovic <ivo.ristovic@gmail.com>
2020-03-01 23:08:16 +0100

    [LuaAdapter] Locals

--- RICC/AST/Builders/Lua/LuaASTBuilder.Statements.cs
+++ RICC/AST/Builders/Lua/LuaASTBuilder.Statements.cs
@@ -18,19 +18,8 @@ namespace RICC.AST.Builders.Lua
         {
             if (ctx.varlist() is { }) {
                 IdentifierListNode vars = this.Visit(ctx.varlist()).As<IdentifierListNode>();
-                ExpressionListNode explist = this.Visit(ctx.explist()).As<ExpressionListNode>();
-                if (explist.Children.Count < vars.Children.Count) {
-                    int missingCount = vars.Children.Count - explist.Children.Count;
-                    IEnumerable<ExpressionNode> missing = Enumerable.Repeat<ExpressionNode>(new LiteralNode(ctx.Start.Line, "nil"), missingCount);
-                    explist = new ExpressionListNode(ctx.Start.Line, explist.Expressions.Concat(missing));
-                }
-                IEnumerable<AssignmentExpressionNode> assignments = vars.Identifiers
-                    .Zip(explist.Expressions)
-                    .Select(tup => new AssignmentExpressionNode(ctx.Start.Line, tup.First, tup.Second));
-                // TODO declare vars if not seen before
-                if (vars.Children.Count == 1) 
-                    return new ExpressionStatementNode(ctx.Start.Line, assignments.First());
-                return new BlockStatementNode(ctx.Start.Line, assignments);
+                ExpressionListNode inits = this.Visit(ctx.explist()).As<ExpressionListNode>();
+                return CreateAssignmentNode(ctx.Start.Line, vars, inits);
             } else if (ctx.functioncall() is { }) {
                 return new EmptyStatementNode(ctx.Start.Line);  // TODO
             } else if (ctx.label() is { }) {
@@ -57,10 +46,44 @@ namespace RICC.AST.Builders.Lua
                     case "function":
                         return new EmptyStatementNode(ctx.Start.Line); // TODO
                     case "local":
-                        return new EmptyStatementNode(ctx.Start.Line); // TODO
+                        if (ctx.children[1].GetText().Equals("function", StringComparison.InvariantCultureIgnoreCase))
+                            return new EmptyStatementNode(ctx.Start.Line);  // TODO
+                        IdentifierListNode vars = this.Visit(ctx.namelist()).As<IdentifierListNode>();
+                        if (ctx.explist() is { }) {
+                            ExpressionListNode inits = this.Visit(ctx.explist()).As<ExpressionListNode>();
+                            return CreateAssignmentNode(ctx.Start.Line, vars, inits);
+                        } else {
+                            IEnumerable<VariableDeclaratorNode> varDecls = vars.Identifiers
+                                .Select(v => new VariableDeclaratorNode(ctx.Start.Line, v))
+                                ;
+
+                            // TODO
+                            var declSpecs = new DeclarationSpecifiersNode(ctx.Start.Line, "local", "object");
+                            var decls = new DeclaratorListNode(ctx.Start.Line, varDecls);
+                            return new DeclarationStatementNode(ctx.Start.Line, declSpecs, decls);
+                        }
                     default:
                         throw new SyntaxException("Invalid statement type.");
                 }
+            }
+
+
+            static ASTNode CreateAssignmentNode(int line, IdentifierListNode vars, ExpressionListNode initializers)
+            {
+                if (initializers.Children.Count < vars.Children.Count) {
+                    int missingCount = vars.Children.Count - initializers.Children.Count;
+                    IEnumerable<ExpressionNode> missing = Enumerable.Repeat<ExpressionNode>(new LiteralNode(line, "nil"), missingCount);
+                    initializers = new ExpressionListNode(line, initializers.Expressions.Concat(missing));
+                }
+
+                IEnumerable<AssignmentExpressionNode> assignments = vars.Identifiers
+                    .Zip(initializers.Expressions)
+                    .Select(tup => new AssignmentExpressionNode(line, tup.First, tup.Second))
+                    ;
+
+                if (vars.Children.Count == 1)
+                    return new ExpressionStatementNode(line, assignments.First());
+                return new BlockStatementNode(line, assignments);
             }
         }
 
@@ -78,5 +101,8 @@ namespace RICC.AST.Builders.Lua
             // TODO
             return new IdentifierNode(ctx.Start.Line, ctx.NAME().GetText());
         }
+
+        public override ASTNode VisitNamelist([NotNull] NamelistContext ctx)
+            => new IdentifierListNode(ctx.Start.Line, ctx.NAME().Select(v => new IdentifierNode(ctx.Start.Line, v.GetText())));
     }
 }
