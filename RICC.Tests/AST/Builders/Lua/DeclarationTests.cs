715d5fdfad0a7a7ff8400895f2fe2dac3b8cf1d7
Ivan Ristovic <ivo.ristovic@gmail.com>
2020-03-01 23:09:56 +0100

    [LuaAdapter-Tests] Begin declaration tests

--- RICC.Tests/AST/Builders/Lua/DeclarationTests.cs
+++ RICC.Tests/AST/Builders/Lua/DeclarationTests.cs
@@ -1,12 +1,17 @@
 ﻿using NUnit.Framework;
-using RICC.AST.Builders.C;
+using RICC.AST.Builders.Lua;
 using RICC.AST.Nodes.Common;
 using RICC.Tests.AST.Builders.Common;
 
 namespace RICC.Tests.AST.Builders.Lua
 {
-    internal sealed class DeclarationTests : DeclarationTestsBase<CASTBuilder>
+    internal sealed class DeclarationTests : DeclarationTestsBase<LuaASTBuilder>
     {
-
+        [Test]
+        public void SimpleDeclarationTest()
+        {
+            this.AssertVariableDeclaration("x = 2", "x", "object", value: 2);
+            this.AssertVariableDeclaration("local y = 2", "y", "object", value: 2);
+        }
     }
 }
