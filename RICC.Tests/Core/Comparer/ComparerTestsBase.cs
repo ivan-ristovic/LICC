1f6c2b2d4369acdb339d2f8f24dbcbfa824539e3
Ivan Ristovic <ivo.ristovic@gmail.com>
2020-03-25 23:57:42 +0100

    [Core-Tests] Comparer and integration skeletons added

---
+++ RICC.Tests/Core/Comparer/ComparerTestsBase.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using RICC.AST.Nodes;
+using RICC.Core;
+
+namespace RICC.Tests.Core.Comparer
+{
+    internal abstract class ComparerTestsBase
+    {
+        protected void Compare(ASTNode src, ASTNode dst, MatchIssues? expectedIssues = null)
+        {
+            MatchIssues issues = new ASTNodeComparer(src, dst).AttemptMatch();
+            expectedIssues ??= new MatchIssues();
+            Assert.That(issues, Is.EqualTo(expectedIssues));
+        }
+    }
+}
